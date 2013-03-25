4a9fddbd163fe8294902c55bd154e5a2ea1964af
Talglys <yottagamer@hotmail.com>
2013-03-25 13:23:41 +0100

    Add Log.cs

---
+++ spacePirates/SpacePirates/Log.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace SpacePirates
+{
+    class Log
+    {
+
+        private Log log; //This is a singleton object
+
+        ArrayList eventLog;
+
+        //Constructor
+        private Log()
+        {
+            eventLog = new ArrayList();
+        }
+
+        /// <summary>
+        /// Gets the singleton object, so it
+        /// can be used.
+        /// </summary>
+        /// <returns></returns>
+        public Log getLog()
+        {
+            if (log == null)
+            {
+                log = new Log();
+            }
+            return log;
+        }
+
+        /// <summary>
+        /// Adds an event to the log. The event should be in String format, and
+        /// should explain an event that just occured. Preferably, some variable
+        /// values should be included, and the message should be short and consise.
+        /// </summary>
+        /// <param name="happening"></param>
+        public void addEvent(String happening)
+        {
+            eventLog.Add(happening);
+        }
+
+    }
+}
