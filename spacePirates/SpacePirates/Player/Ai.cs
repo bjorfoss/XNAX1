648f8f4d587bfcaadc2453d41217a68c2b95b49a
bjorfoss <bjofoss2@hotmail.com>
2013-03-25 14:18:04 +0100

    Human and AI fields and methods

--- spacePirates/SpacePirates/Player/Ai.cs
+++ spacePirates/SpacePirates/Player/Ai.cs
@@ -7,5 +7,16 @@ namespace SpacePirates.Player
 {
     class Ai : Player
     {
+        Ownership ownerLink;
+
+        /// <summary>
+        /// Determine general goal/strategy
+        /// </summary>
+        void MakeGeneralPlan();
+
+        /// <summary>
+        /// Make local decisions supporting the strategy
+        /// </summary>
+        void MakeLocalPlan();
     }
 }
