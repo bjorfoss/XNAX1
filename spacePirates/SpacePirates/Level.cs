ce4cba3a0739176291ddc559bb34323ecc34f6fc
bjorfoss <bjofoss2@hotmail.com>
2013-03-25 13:31:20 +0100

    Add methods to Level and Ownership

--- spacePirates/SpacePirates/Level.cs
+++ spacePirates/SpacePirates/Level.cs
@@ -2,10 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace SpacePirates
 {
     class Level
     {
+        Texture2D background;
+        HashSet<Rectangle> foreground;
+
+        public void Draw(SpriteBatch sb);
     }
 }
--- spacePirates/SpacePirates/Player/Ownership.cs
+++ spacePirates/SpacePirates/Player/Ownership.cs
@@ -2,11 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using SpacePirates.spaceShips;
 
 namespace SpacePirates.Player
 {
     class Ownership
     {
+        Player owner;
+        SpaceShip spaceShip;
 
+        public SpaceShip GetShip() {
+            return spaceShip;
+        }
+
+        public Player GetOwner()
+        {
+            return owner;
+        }
     }
 }
