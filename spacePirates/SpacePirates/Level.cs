eb6788b5fc57ba0dd13a1e495625cbf4491945c1
bjorfoss <bjofoss2@hotmail.com>
2013-04-15 14:45:32 +0200

    Add level limits #18
    * Add IsOutsideLevel to check if a unit has moved outside of bounds
    * Add GetLevelBounds to get the bounds for the level

--- spacePirates/SpacePirates/Level.cs
+++ spacePirates/SpacePirates/Level.cs
@@ -13,7 +13,7 @@ namespace SpacePirates
     {
         private Texture2D background;
         private HashSet<Rectangle> foreground;
-        private Vector2 size;
+        private Rectangle bounds;
         private Vector2 position;
         private Rectangle croppedBackground; //the visible part
 
@@ -22,13 +22,35 @@ namespace SpacePirates
             Level self = this;
 
             self.background = Content.Load<Texture2D>("background");
-            size = new Vector2(background.Width, background.Height);
+            bounds = new Rectangle(0, 0, 30000, 30000);
             position = new Vector2(0, 0);
         }
 
-        public Vector2 GetLevelSize()
+        /// <summary>
+        /// Get a rectangle representing the entire level
+        /// </summary>
+        /// <returns></returns>
+        public Rectangle GetLevelBounds()
         {
-            return size;
+            return bounds;
+        }
+
+        /// <summary>
+        /// Check if a unit is outside of the level
+        /// </summary>
+        /// <param name="unit">The unit to check</param>
+        /// <returns>true if the unit is outside, otherwise false</returns>
+        public bool IsOutsideLevel(Unit unit)
+        {
+            Vector2 position = unit.GetPosition();
+            //Don't bother getting an accurate size for the unit rectangle,
+            //the level check doesn't require that much accuracy
+            Rectangle rect = new Rectangle((int)position.X, (int)position.Y, 10, 10);
+            //The level rectangle intersects with the unit's rectangle
+            if (bounds.Intersects(rect)) {
+                return false;
+            }
+            return true;
         }
 
         private void CalculateLevelCrop(SpriteBatch sb)
